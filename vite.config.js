c2b2bb315cf4f7c756816df8f240634db60b0bc6
Bruno Cantarelli <cantarellibruno2004@gmail.com>
2024-07-09 11:35:05 -0300

    att 09/07

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Tela-Simples-Login-ReactJS/',
+  base: '/tela_login_reactjs/',
   plugins: [react()],
 });
